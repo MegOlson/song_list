4bca4b233c72252bb1d0e1e8f5abf2669ba4ca78
linda luu and Megan Olson <megan@>
2017-09-18 13:16:03 -0700

    pass test to clear list

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,1 +1,11 @@
 require 'spec_helper'
+
+describe(List) do
+  let(:list) { List.new({:name => "Jingle Bells", :id => nil}) }
+  describe(".all") do
+    it("starts off with no lists") do
+      expect(List.all).to eq []
+    end
+  end
+  
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,6 @@ DB = PG.connect({:dbname => "music_test"})
 RSpec.configure do |config|
   config.after(:each) do
     DB.exec("DELETE FROM songs *;")
-    # DB.exec("DELETE FROM quantity *;")
+    DB.exec("DELETE FROM lists *;")
   end
 end
